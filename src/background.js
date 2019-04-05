3cbae8da4b302acf2dff8ec72a988e6a6d102b8c
vishal-vp <21176895+vishal-vp@users.noreply.github.com>
2019-04-05 10:57:33 +0530

    Add multiple tabs state and deactivate on esc keypress

--- src/background.js
+++ src/background.js
@@ -4,18 +4,23 @@ let ports = [];
 let states = {};
 
 function connected(p) {
+  p.isActivated = false;
   ports[p.sender.tab.id] = p;
   p.onDisconnect.addListener(() => {
     // Remove port from list.
     ports.splice(p.sender.tab.id, 1);
   });
+  p.onMessage.addListener((m) => {
+    if (m.action === 'deactivate') {
+      ports[p.sender.tab.id].isActivated = false;
+    }
+  });
 }
 
 browser.runtime.onConnect.addListener(connected);
 
-browser.browserAction.onClicked.addListener(function() {
-  isActivated = !isActivated;
-  ports.forEach(p => {
-    p.postMessage({'isActivated': isActivated});
-  })
+browser.browserAction.onClicked.addListener(function(tab) {
+  port = ports[tab.id];
+  port.isActivated = !port.isActivated;
+  port.postMessage({'isActivated': port.isActivated});
 });
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import $ from 'jquery';
 import invert from 'invert-color';
 import Color from 'color';
 
+const highlightColor = '#00ff00';
 let previousElement, previousBackground, previousCursor;
-let bodyColor = $('body').css('backgroundColor');
 let isActivated = false;
 
 function mouseMoveListener(event) {
@@ -29,27 +29,44 @@ function mouseMoveListener(event) {
   return false;
 }
 
-function elementClickListener(event) {
+function elementClickListener() {
   // Delete the clicked element.
   $(this).css('display', 'none');
   return false;
 }
 
+function deactivateKeyPressListener(event) {
+  // Deactivate extension when esc key is pressed.
+  if (event.keyCode === 27) {
+    myPort.postMessage({'action': 'deactivate'});
+    unbindEvents();
+  }
+}
+
+function bindEvents() {
+  $('*').on('mousemove', mouseMoveListener);
+  $('*').on('click', elementClickListener);
+  $(document).on('keyup', deactivateKeyPressListener);
+}
+
+function unbindEvents() {
+  $('*').off('mousemove', mouseMoveListener);
+  $('*').off('click', elementClickListener);
+  if (previousElement) {
+    // Restore previous element appearance.
+    previousElement.css('backgroundColor', previousBackground)
+                   .css('cursor', previousCursor);
+    previousElement = previousBackground = previousCursor = null;
+  }
+}
+
 // Setup communication between with background script.
-var myPort = browser.runtime.connect({name:"port-from-cs"});
+const myPort = browser.runtime.connect();
 
 myPort.onMessage.addListener(function(message) {
   if (message.isActivated) {
-    $('*').on('mousemove', mouseMoveListener);
-    $('*').on('click', elementClickListener);
+    bindEvents();
   } else {
-    $('*').off('mousemove', mouseMoveListener);
-    $('*').off('click', elementClickListener);
-    if (previousElement) {
-      // Restore previous element appearance.
-      previousElement.css('backgroundColor', previousBackground)
-                     .css('cursor', previousCursor);
-      previousElement = previousBackground = previousCursor = null;
-    }
+    unbindEvents();
   }
 });
